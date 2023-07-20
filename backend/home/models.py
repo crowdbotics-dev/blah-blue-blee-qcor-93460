447a39ff5187aa617d79259a2436419be86ce89a
crowdbotics-dev <engineering@crowdbotics.com>
2023-07-20 20:22:47 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -4,3 +4,4 @@ class Test(models.Model):
     'Generated Model'
     test = models.BigIntegerField()
     rr = models.BigIntegerField(null=True,blank=True,)
+    ddbbb = models.TimeField(auto_now=True,null=True,blank=True,)
